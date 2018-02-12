2df56be039410b7a9eed28eb96004d3a9d80c5dd
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-02-11 20:43:42 -0500

    created database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured, date)VALUES('burger1',FALSE,current_timestamp);
+INSERT INTO burgers (burger_name, devoured, date)VALUES('burger2',FALSE,current_timestamp);
+INSERT INTO burgers (burger_name, devoured, date)VALUES('burger3',FALSE,current_timestamp);
