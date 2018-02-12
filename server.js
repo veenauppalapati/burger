63486f6e53d794c3121125342fcadae6b234efaa
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-02-11 19:43:38 -0500

    introducing and setting handlebars as the new render engine

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var bodyParser = require ('body-parser');
 var methodOverride = require('method-override');
+var exphb = require('express-handlebars');
+
 
 var app = express();
 var PORT = 3000;
 
+
 app.use(bodyParser.urlencoded({extended:false}));
 apps.use(bodyParser.json());
+
+app.engine('handlebars', exphb({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.listen(PORT, function(){
     console.log("Listening to : "+ PORT);
