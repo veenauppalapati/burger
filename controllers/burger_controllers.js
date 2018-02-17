e3b014cd5bdd27af93d20aa9db4368883158a8ef
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-02-17 00:33:05 -0500

    lists are added to html

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -4,3 +4,19 @@ var router = express.Router();
 
 //Import the model (cat.js) to use its database functions.
 var burger = require("../models/burger.js");
+
+router.post('/burger', function(req, res){
+    function()
+})
+
+app.post("/todos", function(req, res) {
+    connection.query("INSERT INTO plans (plan) VALUES (?)", [req.body.plan], function(err, result) {
+      if (err) {
+        return res.status(500).end();
+      }
+  
+      // Send back the ID of the new todo
+      res.json({ id: result.insertId });
+      console.log({ id: result.insertId });
+    });
+  });
